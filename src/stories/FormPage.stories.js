8a5386a8c5efb9029738350224a6dc16a7ce29d5
Kiwane Garner <kiwanegarner012@yahoo.com>
2019-11-01 00:13:11 -0400

    added the Email, FullName, and Thank you pages to storybook

--- src/stories/FormPage.stories.js
+++ src/stories/FormPage.stories.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
 import '../App.css';
+import '../css/storybookcss/styles.css';
+
+import  EmailForm from '../components/form/UserEmailForm';
+import FullNameForm from '../components/form/UserFullNameForm.';
+import ThankYouPage from '../components/form/ThankYouPage';
 
 
 
@@ -9,29 +13,47 @@ export default {
   title: 'Form Pages',
 };
 
-export const EmailForm= () => {
-  return (
-    <div>
-      <p>This is the complete email form that users will see first once the page is loaded.</p>
+// export const EmailForm = () => {
+//   return (
+//     <div>
+//       <p>This is the complete email form that users will see first once the page is loaded.</p>
 
-      <div id="Signup-Form-Container" style={{margin: "auto", height: "250px"}}>
-                    <form >
-                        <div className="signup-form">
-                        <h1 style={{fontFamily: "Elaine_D02", color: "#B6FDBF", fontSize: "60px"}}>join the list</h1>
-                        <p style={{fontSize: "20px", fontFamily: "Azo-Sans",  textAlign: "center"}}> SIGN UP FOR THE TLC NEWSLETTER.</p>
-                        <input style={{ display: "block", width: "50%", padding: "5px", borderRadius: "5px", margin: "70px auto auto"}} placeholder="enter email address" type="text" />
-                        <div style={{textAlign: "center", fontFamily: "Azo-Sans"}}>
-                          <button onClick={action('clicked the submit button')} style={{padding: "5px 30px", borderRadius: "5px", backgroundColor: "red", border: "none", color: "white"}}>Submit</button>
-                        </div>
-                        <input style={{ display: "block",margin: "auto"}}  type="checkbox"/>
-                        <div style={{textAlign: "center", fontFamily: "Azo-Sans"}}>
-                          <small>I agree to receive information from Discovery Communications 
-                            <span style={{display: "block"}}> in accordance with the following Privacy Policy</span>
-                          </small>
-                        </div>
-                        </div>
-                    </form>
-            </div>
-    </div>
-  )
-}
+//       <div id="Signup-Form-Container" style={{margin: "auto", height: "250px"}}>
+//                     <form >
+//                         <div className="signup-form">
+//                         <h1 style={{fontFamily: "Elaine_D02", color: "#B6FDBF", fontSize: "60px"}}>join the list</h1>
+//                         <p style={{fontSize: "20px", fontFamily: "Azo-Sans",  textAlign: "center"}}> SIGN UP FOR THE TLC NEWSLETTER.</p>
+//                         <input style={{ display: "block", width: "50%", padding: "5px", borderRadius: "5px", margin: "70px auto auto"}} placeholder="enter email address" type="text" />
+//                         <div style={{textAlign: "center", fontFamily: "Azo-Sans"}}>
+//                           <button onClick={action('clicked the submit button')} style={{padding: "5px 30px", borderRadius: "5px", backgroundColor: "red", border: "none", color: "white"}}>Submit</button>
+//                         </div>
+//                         <input style={{ display: "block",margin: "auto"}}  type="checkbox"/>
+//                         <div style={{textAlign: "center", fontFamily: "Azo-Sans"}}>
+//                           <small>I agree to receive information from Discovery Communications 
+//                             <span style={{display: "block"}}> in accordance with the following Privacy Policy</span>
+//                           </small>
+//                         </div>
+//                         </div>
+//                     </form>
+//             </div>
+//     </div>
+//   )
+// }
+
+export const Email = () => (
+  <div>
+    <EmailForm />
+  </div>
+);
+
+export const FullName= () => (
+  <div>
+    <FullNameForm  />
+  </div>
+);
+
+export const ThankYou = () => (
+  <div>
+    <ThankYouPage  />
+  </div>
+);
