8f2fc9cb8b9c208289e1c068154af8cc2bab0b08
Kiwane Garner <kiwanegarner012@yahoo.com>
2019-11-01 22:25:53 -0400

    Added props to each forms story to complete the story UI/ design

--- src/stories/FormPage.stories.js
+++ src/stories/FormPage.stories.js
@@ -13,47 +13,32 @@ export default {
   title: 'Form Pages',
 };
 
-// export const EmailForm = () => {
-//   return (
-//     <div>
-//       <p>This is the complete email form that users will see first once the page is loaded.</p>
-
-//       <div id="Signup-Form-Container" style={{margin: "auto", height: "250px"}}>
-//                     <form >
-//                         <div className="signup-form">
-//                         <h1 style={{fontFamily: "Elaine_D02", color: "#B6FDBF", fontSize: "60px"}}>join the list</h1>
-//                         <p style={{fontSize: "20px", fontFamily: "Azo-Sans",  textAlign: "center"}}> SIGN UP FOR THE TLC NEWSLETTER.</p>
-//                         <input style={{ display: "block", width: "50%", padding: "5px", borderRadius: "5px", margin: "70px auto auto"}} placeholder="enter email address" type="text" />
-//                         <div style={{textAlign: "center", fontFamily: "Azo-Sans"}}>
-//                           <button onClick={action('clicked the submit button')} style={{padding: "5px 30px", borderRadius: "5px", backgroundColor: "red", border: "none", color: "white"}}>Submit</button>
-//                         </div>
-//                         <input style={{ display: "block",margin: "auto"}}  type="checkbox"/>
-//                         <div style={{textAlign: "center", fontFamily: "Azo-Sans"}}>
-//                           <small>I agree to receive information from Discovery Communications 
-//                             <span style={{display: "block"}}> in accordance with the following Privacy Policy</span>
-//                           </small>
-//                         </div>
-//                         </div>
-//                     </form>
-//             </div>
-//     </div>
-//   )
-// }
-
 export const Email = () => (
   <div>
-    <EmailForm />
+      <p>
+          By reusing my form atom components, props, and the global state of the signup form wrapper element,
+          I am able to create an email sign up form as shown here. This form allows me to store or transfer a user's email.
+     </p>
+    <EmailForm newHeader = "join the list" />
   </div>
 );
 
 export const FullName= () => (
   <div>
-    <FullNameForm  />
+    <p>
+          By reusing my form atom components, props, and the global state of the signup form wrapper element,
+          I am able to create a full name sign up form as shown here. This form allows me to store or transfer a user's first and last name.
+    </p>
+    <FullNameForm  newHeader = "join the list"  />
   </div>
 );
 
 export const ThankYou = () => (
   <div>
-    <ThankYouPage  />
+      <p>
+          By reusing my form atom components, props, and the global state of the signup form wrapper element,
+          I am able to create a Thank you page to show that the form has been completed and submitted.
+      </p>
+    <ThankYouPage  newHeader = "congratulations" />
   </div>
 );
