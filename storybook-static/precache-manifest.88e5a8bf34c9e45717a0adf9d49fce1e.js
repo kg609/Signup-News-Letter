e972e49a6a14bcd0e1684194aa518a3ae06aae80
Kiwane Garner <kiwanegarner012@yahoo.com>
2019-11-13 21:21:05 -0500

    deployed newsletter  storybook to netlify

---
+++ storybook-static/precache-manifest.88e5a8bf34c9e45717a0adf9d49fce1e.js
@@ -0,0 +1,51 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717fe075544a6237fb1157e28fecee77",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.96a49a1a57133d5569f7.bundle.js"
+  },
+  {
+    "revision": "1d37e1bb567a1c46db1c",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.96a49a1a57133d5569f7.bundle.js"
+  },
+  {
+    "revision": "0e7a9d1bb0b51587bc868e9549d95195",
+    "url": "static/media/Azo-Sans.0e7a9d1b.eot"
+  },
+  {
+    "revision": "5cd5dd7d40c2c83c1e87e3d95815b844",
+    "url": "static/media/Azo-Sans.5cd5dd7d.svg"
+  },
+  {
+    "revision": "88ed9b8a1c42e366d81ed994320ac23b",
+    "url": "static/media/Azo-Sans.88ed9b8a.woff"
+  },
+  {
+    "revision": "b8344010b32da9f6eff4c22cf377fb03",
+    "url": "static/media/Azo-Sans.b8344010.ttf"
+  },
+  {
+    "revision": "3aadfe76191eb3880272658959119c66",
+    "url": "static/media/Elaine_D02.3aadfe76.svg"
+  },
+  {
+    "revision": "7b7d4e516c3664470125ccec36a895db",
+    "url": "static/media/Elaine_D02.7b7d4e51.ttf"
+  },
+  {
+    "revision": "d817e82f00ca0e949be73908057c9c7e",
+    "url": "static/media/Elaine_D02.d817e82f.eot"
+  },
+  {
+    "revision": "f55d13a16f31a83a508e557dc19c82df",
+    "url": "static/media/Elaine_D02.f55d13a1.woff"
+  },
+  {
+    "url": "vendors~main.96a49a1a57133d5569f7.bundle.js"
+  }
+]);
