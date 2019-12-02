4c5a0294fc177127ac1589b6e7e1c4ba9c4daa10
Kiwane Garner <kiwanegarner012@yahoo.com>
2019-12-01 21:48:12 -0500

    updated storybook pages

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
 
-export default {
-  title: 'Button',
-};
+// export default {
+//   title: 'Button',
+// };
 
 export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
 
